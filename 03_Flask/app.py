c3a204bf2d2af5ac0397704b1e126f511061a1a6
Yeongseon Kim <son960311@naver.com>
2019-10-23 14:49:37 +0900

    191023 | 오후수업'

--- 03_Flask/app.py
+++ 03_Flask/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template ,request
+import random
 app = Flask(__name__)
 
 @app.route('/')
@@ -58,6 +59,31 @@ def naver():
 @app.route('/google')
 def google():
     return render_template('google.html')
+
+# 사용자로부터 이름 입력 받을 Form 페이지!
+@app.route('/vonvon')
+def vonvon():
+    return render_template('vonvon.html')
+
+# 전달받은 이름을 기준으로 넘겨줄 각종 정보를 가공하는 로직!
+@app.route('/godmademe')
+def godmademe():
+
+    # 1. 사용자가 입력한 데이터를 가져온다.
+    name = request.args.get('user_name')
+    
+    # 2. 사용자에게 보여줄 여러가지 재밌는 특성들 리스트를 만든다.
+    first_list = ['잘생김','못생김','많이 못생김','많이 잘생김','못났음','모태이쁨', '모태못생김']
+    second_list = ['자신감','귀찮음','쑥스러움','열정적임']
+    third_list = ['허세','물욕','식욕','똘끼','애교']
+
+    # 3. 리스트에서 랜덤으로 하나씩을 꺼낸다.
+    first = random.choice(first_list)
+    second = random.choice(second_list)
+    third = random.choice(third_list)
+
+    # 4. 가공한 정보를 템플릿에 담아서 사용자에게 보여준다.
+    return render_template('godmademe.html',name_html=name, first_html=first, second_html=second, third_html=third )
 
 # end of file!!!
 # debug 모드를 활성화해서 서버 새로고침을 생략한다.
