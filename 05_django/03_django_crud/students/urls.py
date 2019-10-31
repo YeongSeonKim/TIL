49d9ff27570b99f6b01bc8c0c422adb76ef5a17d
Yeongseon Kim <son960311@naver.com>
2019-10-31 16:29:50 +0900

    191031 | Django

--- 05_django/03_django_crud/students/urls.py
+++ 05_django/03_django_crud/students/urls.py
@@ -5,4 +5,11 @@ app_name = 'students'
 
 urlpatterns = [
     path('',views.index , name='index'),
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
+    path('<int:student_pk>/', views.detail, name='detail'),
+    path('<int:student_pk>/delete/', views.delete, name='delete'),
+    path('<int:student_pk>/edit/', views.edit, name='edit'),
+    path('<int:student_pk>/update/', views.update, name='update'),
+
 ]
--- 05_django/03_django_crud/students/views.py
+++ 05_django/03_django_crud/students/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Student
 
 # Create your views here.
+
+# index 페이지 - 학생들 목록
 def index(request):
-    return render(request, 'students/index.html')
+    students = Student.objects.all()[::-1]
+    context = {'students':students}
+    return render(request, 'students/index.html', context)
+
+# CREATE
+def new(requset):
+    return render(requset, 'students/new.html')
+
+def create(request):
+    name = request.POST.get('name')
+    age = request.POST.get('age')
+    email = request.POST.get('email')
+
+    student = Student(name=name, age=age, email=email)
+    student.save()
+
+    return redirect('students:detail', student.pk)
+
+# detail 페이지 - 학생 상세정보 
+def detail(request, student_pk):
+    student = Student.objects.get(pk = student_pk)
+    context = {'student':student}
+    return render(request, 'students/detail.html', context)
+
+# DELETE
+# delete 페이지
+def delete(request, student_pk):
+    student = Student.objects.get(pk = student_pk)
+    student.delete()
+    return redirect('students:index')
+
+# UPDATE
+def edit(request, student_pk):
+    student = Student.objects.get(pk = student_pk)
+    context = {'student':student}
+    return render(request, 'students/edit.html', context)
+
+def update(request, student_pk):
+    student = Student.objects.get(pk = student_pk)
+    
+    student.name = request.POST.get('name')
+    student.age = request.POST.get('age')
+    student.email = request.POST.get('email')
+    
+    student.save()
+
+    return redirect('students:detail', student_pk)
